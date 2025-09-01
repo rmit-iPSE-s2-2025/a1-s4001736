a2bcec272a8592510200b7e630030e518c9572d1
Jewel2323 <sereyrothna.sok@gmail.com>
2025-09-01 22:50:25 +1000

    Initial Commit

---
+++ SelfieMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SelfieMate
+//
+//  Created by Julie Sok on 1/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SelfieMate/SelfieMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfieMateApp.swift
+//  SelfieMate
+//
+//  Created by Julie Sok on 1/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct SelfieMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
